54cb3da8a38fa4363ca711316fd70da108980456
Sanoop-PR <sanooppr60@gmail.com>
2024-09-06 13:14:21 +0530

    card

--- app/src/Components/Admin/AdminHome.jsx
+++ app/src/Components/Admin/AdminHome.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import mailImg from '../../assets/logo/usermail.png'
+import { IoIosArrowForward } from "react-icons/io";
+
 
 const AdminHome = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className='p-4'>
+          <h1 className='text-2xl'><b>Hi, Charan!</b></h1>
+          <p>Let’s help to find your dream career</p>
+          <h1 className='text-xl'><b>To do list(1)</b></h1>
+          <section className="grid grid-cols-6 bg-white">
+            <div className=' col-span-1 grid place-content-center '>
+              <img src={mailImg} alt="" className='w-6' />
+            </div>
+            <div className=' col-span-4 '>
+              <b>Invitation to apply</b>
+              <p>Persists Ventures</p>
+              <b className='text-red-500'>Invitation expires in 3 days</b>
+            </div>
+            <div className=' col-span-1 grid place-content-center'>
+              <IoIosArrowForward/>
+            </div>
+          </section>
+      </section>
+    </>
   )
 }
 
--- app/src/Components/Common/StudentCard.jsx
+++ app/src/Components/Common/StudentCard.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import personImg from "../../assets/vector/person.png";
+import { IoIosArrowForward } from "react-icons/io";
 
 const StudentCard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div class="relative flex flex-col mt-6 bg-white border border-slate-200 rounded-lg w-full shadow-lg">
+        <div class="p-4 grid grid-cols-3">
+          <div class="mb-2 text-slate-800 text-xl font-semibold grid content-around">
+            <img src={personImg} alt="" className=" w-20 h-20 " />
+          </div>
+          <div class="text-slate-600 leading-normal font-light col-span-2">
+            <p className="font-medium">Mentor Name: Charan</p>
+            <p className="font-medium">Qualification: Btech CSE</p>
+            <p className="font-medium">Email: charan@gmail.com</p>
+            <p className="font-medium">Conatct No.: 1234567890</p>
+            <p className="font-medium">Expertice: Software Engineer</p>
+            <p className="font-medium">Industry: IT & development</p>
+          </div>
+        </div>
+        <div class="mx-3 border-t border-slate-200 pb-3 pt-2 px-1 flex justify-end">
+          <b class="text-sm text-blue-500 flex">
+            View Details <IoIosArrowForward />
+          </b>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default StudentCard
+export default StudentCard;
